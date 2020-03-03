42a8ac1e1b3a7d3b5f8d6553fc5d8bbd575c7e60
Cédric <cedric.terrier@epfl.ch>
2020-03-03 16:54:12 +0100

    some layout

--- problem_set_1/Pytest_Gab.py
+++ problem_set_1/Pytest_Gab.py
@@ -50,7 +50,7 @@ labels = pd.cut(data_cleaned['tenure'], bins, labels=group_names)
 # Grouping the data by labels and gender
 grouped = data_cleaned.groupby([labels, 'gender'])
 Gender_loyalty = grouped.size().unstack()
-Gender_loyalty.plot.barh(title='Loyalty by gender')
+Gender_loyalty.plot.bar(title='Loyalty by gender')
 print(Gender_loyalty)
 
 # From this plot one can observe the distribution of clients within the loyalty classification made above 
@@ -66,11 +66,9 @@ PhoneService_norm = grouped2['PhoneService_int'].sum()/grouped2.size()
 normed_infos = pd.DataFrame(PhoneService_norm, columns=['Has_PhoneService'])
 
 
-
 # Getting the proportion on people with internet service
-
-My_dict2={'DSL':1,'Fiber optic':1,'No':0}
-data_cleaned['InternetService_int']=data_cleaned['InternetService'].map(My_dict2)
+My_dict2 = {'DSL': 1, 'Fiber optic': 1, 'No': 0}
+data_cleaned['InternetService_int'] = data_cleaned['InternetService'].map(My_dict2)
 
 InternetService_norm = grouped2['InternetService_int'].sum()/grouped2.size()
 normed_infos['Has_InternetService'] = InternetService_norm
@@ -86,10 +84,10 @@ fig = plt.figure()
 ax = fig.add_subplot(1, 1, 1)
 ax.set(**props)
 ax.legend(loc='best')
-normed_infos.plot.bar(ax=ax,rot=30)
+normed_infos.plot.bar(ax=ax, rot=30)
 
 # Analyze the internet services
-data_cleaned_with_internet = data_cleaned[data_cleaned['InternetService']!= 'No']
+data_cleaned_with_internet = data_cleaned[data_cleaned['InternetService'] != 'No']
 labels2 = pd.cut(data_cleaned_with_internet['tenure'], bins, labels=group_names)
 grouped3 = data_cleaned_with_internet.groupby(labels2)
 
@@ -97,13 +95,13 @@ internet_services_df = normed_infos.copy()
 
 for x in data_cleaned.columns[9:15]:
     new_col = x + '_int'
-    data_cleaned_with_internet[new_col]=data_cleaned_with_internet[x].map(My_dict)
+    data_cleaned_with_internet[new_col] = data_cleaned_with_internet[x].map(My_dict)
     
     current_norm = grouped3[new_col].sum()/grouped3.size()
     internet_services_df['Has_'+x] = current_norm
     
 
-internet_services_df.drop(columns=['Has_InternetService','Has_PhoneService'],inplace=True)
+internet_services_df.drop(columns=['Has_InternetService', 'Has_PhoneService'], inplace=True)
 
 props2 = {
     'title': 'Percent of clients who have different internet services per loyalty',
@@ -115,30 +113,25 @@ fig = plt.figure()
 ax = fig.add_subplot(1, 1, 1)
 ax.set(**props)
 ax.legend(loc='best')
-internet_services_df.plot.bar(ax=ax,rot=30)
+internet_services_df.plot.bar(ax=ax, rot=30)
 
 # money spent per loyalty
 
-data_cleaned['Labels']=labels
+data_cleaned['Labels'] = labels
 fig = plt.figure()
-sns.barplot(x='Labels', y='MonthlyCharges',data=data_cleaned)
+sns.barplot(x='Labels', y='MonthlyCharges', data=data_cleaned)
 sns.set(style="whitegrid")
 
 # Do women spend more money ?
 
 fig = plt.figure()
-sns.catplot(x='Labels', y='MonthlyCharges',hue='gender',data=data_cleaned)
+sns.catplot(x='Labels', y='MonthlyCharges', hue='gender', data=data_cleaned)
 sns.set(style="whitegrid")
 
 # Other representation 
 
 fig = plt.figure()
-sns.catplot(x='Labels', y='MonthlyCharges', hue='gender',kind="bar",data=data_cleaned)
+sns.catplot(x='Labels', y='MonthlyCharges', hue='gender', kind="bar", data=data_cleaned)
 sns.set(style="whitegrid")
 
 plt.show()
-
-
-
-
-
